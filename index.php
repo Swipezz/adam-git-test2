3f9dadd6e8957cc84821379b285197530cc96de9
Swipezz <adamnoverian@gmail.com>
2023-12-09 22:50:01 +0700

    tambah list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>TEsting</h1>
     <p>Lorem ipsus dolor sit amet, consectetur adipisicing elit. Illum itaque sunt, magnam consectetur culpa deserunt aut veniam, doloribus voluptates maxime perspiciatis eaque ducimus exercitationem voluptatum nisi molestiae! Nemo, officiis debitis?</p>
+    <ul>
+        <li>Halo</li>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
